8c5efcd5d288f66b3e4cb957f59c73241e75193d
nkaihong <nkaihong@163.com>
2018-11-09 21:41:53 +0800

    初始化提交所有代码到远程仓库

---
+++ menu/src/main/java/org/fkjava/menu/MenuConfig.java
@@ -0,0 +1,16 @@
+package org.fkjava.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("org.fkjava")
+@EnableJpaRepositories
+public class MenuConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuConfig.class, args);
+	}
+}
